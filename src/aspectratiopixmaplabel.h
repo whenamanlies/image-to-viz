0453dc0a28abe896f87ae3dd4087fe4fc6173347
Dmitry Chirkov <dmitry.chirkov@gmail.com>
2016-10-13 11:34:42 -0700

    Initial code commit

---
+++ src/aspectratiopixmaplabel.h
@@ -0,0 +1,23 @@
+#ifndef ASPECTRATIOPIXMAPLABEL_H
+#define ASPECTRATIOPIXMAPLABEL_H
+
+#include <QLabel>
+#include <QPixmap>
+#include <QResizeEvent>
+
+class AspectRatioPixmapLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit AspectRatioPixmapLabel(QWidget *parent = 0);
+    virtual int heightForWidth(int width) const;
+    virtual QSize sizeHint() const;
+    QPixmap scaledPixmap() const;
+public slots:
+    void setPixmap (const QPixmap&);
+    void resizeEvent(QResizeEvent *event);
+private:
+    QPixmap pix;
+};
+
+#endif // ASPECTRATIOPIXMAPLABEL_H
